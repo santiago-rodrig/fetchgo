81e97e2285dfa1976f87f2a09d595eca013ff787
santiago-rodrig <santo1996.29@gmail.com>
2021-03-03 12:46:26 -0400

    first implementation

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
 func main() {
-	println("Not implemented")
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+
+		defer resp.Body.Close()
+		b, err := ioutil.ReadAll(resp.Body)
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("%s", b)
+	}
 }
